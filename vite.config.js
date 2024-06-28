975955e9255f71e9a01b900e95920efed39e7bd5
JBalkobsn <jakob_baltes@icloud.com>
2024-06-28 21:18:49 +0000

    First commit from codeserver

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    outDir: "/var/www/clients/client0/web1/web", // Ersetze 'mein-ausgabe-verzeichnis' durch deinen gewünschten Pfad
+  },
   plugins: [react()],
-})
+});
